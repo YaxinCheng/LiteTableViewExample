00f86d44080598d98e55c6b99fdd47d501bc964b
Yaxin Cheng <yaxin.cheng@dal.ca>
2018-10-10 17:11:34 -0400

    Initial Commit

---
+++ LiteTable/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LiteTable
+//
+//  Created by Yaxin Cheng on 2018-10-10.
+//  Copyright © 2018 Yaxin Cheng. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
